7dad2b4d2bad029cbdebdcd713f8c7e66f3c6d65
Xin Wen <1406351146@qq.com>
2022-07-02 17:02:25 +0800

    '哦豁'

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,11 @@
-const {Client} = require('pg')
+const { Client } = require('pg')
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
     database: 'Double-World-Class_Project',
-    password: 'zt123456',
+    password: 'root',
     port: 5432,
 })
 client.connect();
-
 
 module.exports = client
