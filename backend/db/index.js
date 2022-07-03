d86be5e9d4e68cb0e3a4ca327fd651d4c726c10c
JasonLou99 <42426943+JasonLou99@users.noreply.github.com>
2022-07-03 13:32:56 +0800

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,17 +1,10 @@
 const { Client } = require('pg')
 const client = new Client({
     user: 'postgres',
-<<<<<<< HEAD
-    host: 'localhost',
-    database: 'shuangyiliu',
-    password: '12345',
-    port: 5432,
-=======
     host: '222.204.6.192',
     database: 'syl_proj',
     password: 'work@good114',
     port: 12315,
->>>>>>> d89dafd1f74c465459b9cbd4a9237b863f0a7ca4
 })
 client.connect();
 
