a2695f889c6f578da6658ee4ed1340f6f100b90e
Xin Wen <54056797+QWQyyy@users.noreply.github.com>
2022-07-03 11:09:19 +0800

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg')
 const client = new Client({
     user: 'postgres',
-    host: 'localhost',
-    database: 'Double-World-Class_Project',
-    password: 'zt123456',
-    port: 5432,
+    host: '222.204.6.192',
+    database: '???',
+    password: 'work@good114',
+    port: 12315,
 })
 client.connect();
 
