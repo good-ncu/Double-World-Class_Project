c8705af594b0e274707bc94c578a702d513ea534
JasonLou99 <lou1124858363@qq.com>
2022-07-03 19:25:41 +0800

    test

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,9 +1,9 @@
 const { Client } = require('pg') 
  const client = new Client({ 
- user: '', 
- host: '', 
- database: '', 
- password: '', 
+ user: 'postgres', 
+ host: '192.168.0.202', 
+ database: 'postgres', 
+ password: 'just4good', 
  port: 5432, 
  }) 
  client.connect(); 
