86bbf14514aff3c97432aed6e65b03a0ead0b1fb
Xin Wen <54056797+QWQyyy@users.noreply.github.com>
2022-07-03 13:38:05 +0800

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,7 +3,7 @@ const client = new Client({
     user: 'postgres',
     host: '222.204.6.192',
     database: 'syl_proj',
-    password: 'work@good114',
+    password: '',
     port: 12315,
 })
 client.connect();
