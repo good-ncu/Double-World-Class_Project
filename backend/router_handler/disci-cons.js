b1c506e8dd40c6eb5cba906178c18bcd341274b8
JasonLou99 <lou1124858363@qq.com>
2022-07-04 16:01:56 +0800

    学科影响力

--- backend/router_handler/disci-cons.js
+++ backend/router_handler/disci-cons.js
@@ -12,7 +12,7 @@ const expressJWT = require('express-jwt')
 const async = require('async')
 
 // 学科填报的处理函数
-exports.sub = function(req, res){
+exports.disci_eval_situation_sub = function(req, res){
 
     // 接收表单数据
     const submit_info = req.body.data
@@ -40,6 +40,36 @@ exports.sub = function(req, res){
     }
     res.send({ status: 0, message: '填报成功' })
 }   
+
+// 学科影响力情况处理函数 1-1-3
+exports.disci_influence_sub = function(req,res){
+    // 接收表单数据
+    const submit_info = req.body.data_1_1_3
+    console.log(submit_info);
+    // 获取token中的user信息
+    user=req.user
+    for(let i=0,len=submit_info.length;i<len;i++){ 
+        const sql = "INSERT INTO discipline_influ(id, univ_code, discipline_code, year, rank_type, rank, flag, path) VALUES ($1, $2, $3, $4, $5, $6, NULL, NULL)"
+        const strUUID = uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+        const strUUID2 = strUUID.replace(/-/g, '');       // 去掉-字符
+
+            // 执行SQL
+        client.query(sql,[strUUID2,user.univ_code, user.discipline_code, submit_info[i].year, submit_info[i].rank_type, submit_info[i].rank],(err,results)=>{
+        // 执行 SQL 语句失败
+        if (err) return res.send({ status: 1, message: err.message })
+            console.log("disci_influence_sub sql执行成功");
+            // SQL 语句执行成功，但影响行数不为 1
+            // console.log(results.rowCount)
+        if (results.rowCount !== 1) {
+            return res.send({ status: 1, message: '填报成功失败，请稍后再试！' })
+        }
+        // 填报成功
+
+        })
+
+    }
+    res.send({ status: 0, message: '填报成功' })
+}
 
 exports.query_is_time=function(req,res){
     // 接收表单数据
