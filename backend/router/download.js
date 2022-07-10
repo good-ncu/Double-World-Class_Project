ab4f1d3e26e8d384e185bf5592993d3e574d1380
JasonLou99 <lou1124858363@qq.com>
2022-07-10 20:33:50 +0800

    -

--- backend/router/download.js
+++ backend/router/download.js
@@ -4,6 +4,6 @@ const router = express.Router()
 // 导入用户路由处理函数对应的模块
 const download_handler = require('../router_handler/download')
 
-router.get('/download',download_handler.download_excels)
+router.post('/download',download_handler.download_excels)
 
 module.exports = router
