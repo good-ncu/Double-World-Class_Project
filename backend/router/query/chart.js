c44872de45deb557212571ea8e03733ad77e2c9a
JasonLou99 <lou1124858363@qq.com>
2022-07-11 16:43:38 +0800

    -

--- backend/router/query/chart.js
+++ backend/router/query/chart.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const chart_handler = require('../../router_handler/query/chart')
 
  // 省厅查看 学科评估情况
-router.post('/discipline_eval_gov', chart_handler.discipline_eval_gov)
+router.post('/discipline-eval-gov', chart_handler.discipline_eval_gov)
  // 学校查看 学科评估情况
- router.post('/discipline_eval_school', chart_handler.discipline_eval_school)
+ router.post('/discipline-eval-school', chart_handler.discipline_eval_school)
 
 module.exports = router
