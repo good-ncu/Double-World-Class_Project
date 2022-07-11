5c559b4690817e3f9c2aff62b863532a97cd066a
ZengTao <taozeng66@outlook.com>
2022-07-11 19:41:04 +0800

    第一个表的代码

--- backend/router_handler/query/chart.js
+++ backend/router_handler/query/chart.js
@@ -1,8 +1,30 @@
+// 导入数据库操作模块
+const client = require('../../db/index')
 // 省厅查看 学科评估情况
 exports.discipline_eval_gov = function(req,res) {
-    res.send({
-        mes:"00"
-    })
+
+    userinfo = req.user
+    sql = `select discipline_eval_turn,discipline_eval_result,count(discipline_eval_result) from discipline_eval group by discipline_eval_result ,discipline_eval_turn`
+    client.query(sql, function (err, results) {
+        if (err) {
+            // 异常后调用callback并传入err
+            res.send({
+                status: 1,
+                message: err.message
+            })
+        } else if (results.rowCount == 0) {
+            // 当前sql查询为空，则返回填报提示
+            res.send({
+                status: 0,
+                message: "该表格无信息"
+            })
+        } else {
+            res.send({
+                status: 0,
+                data: results.rows
+            })
+        }
+    });
 }
 
 // 学校查看 学科评估情况
