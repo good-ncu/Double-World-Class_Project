babd6fdee9e0a6a914c5fc4ba7f99067a6b152ac
ZengTao <taozeng66@outlook.com>
2022-07-14 15:04:39 +0800

    院校综合考评 	modified:   backend/app.js 	modified:   backend/router/query/gov_query_school_disc.js 	modified:   backend/router_handler/query/gov_query_school_disc.js

--- backend/router/query/gov_query_school_disc.js
+++ backend/router/query/gov_query_school_disc.js
@@ -1,11 +1,15 @@
  const express = require('express')
  const router = express.Router()
  
+ 
  // 导入并使用平台首页 培养拔尖创新人才 路由处理函数对应的模块
  const govl_query_scdis_handler = require('../../router_handler/query/gov_query_school_disc.js')
  
+ //  省厅 一流大学，一流学科  下拉菜单的内容
  router.post('/govl-query-scdis-handler', govl_query_scdis_handler.gov_query_school_disc)
 
+ //  省厅的首页    学校 及其对应 学科  的展示
+router.post('/gov-query-school-disc-eval', govl_query_scdis_handler.gov_query_school_disc_eval)
  
 
 
--- backend/router_handler/query/gov_query_school_disc.js
+++ backend/router_handler/query/gov_query_school_disc.js
@@ -1,7 +1,7 @@
 
 // 导入数据库操作模块
 const client = require('../../db/index')
-// 省厅查看 第四、五、六学科评估情况 （柱状图对比数据）
+// 省厅 一流大学，一流学科  下拉菜单的内容
 exports.gov_query_school_disc = function(req,res) {
     userinfo = req.user
     sql = `SELECT tag.subtag_name,univ_discipline.univ_code,univ_discipline.univ_name,univ_discipline.discipline_code,univ_discipline.discipline_name
@@ -50,4 +50,40 @@ exports.gov_query_school_disc = function(req,res) {
     });
 
 }
+
+
+
+// 省厅 学校 及其对应 学科  的展示
+exports.gov_query_school_disc_eval = function(req,res) {
+    userinfo = req.user
+    sql = `SELECT tag.subtag_name,univ_discipline.univ_code,univ_discipline.univ_name,univ_discipline.discipline_code,univ_discipline.discipline_name
+    FROM univ_discipline
+    INNER JOIN tag ON univ_discipline.tag_id = tag.tag_id
+    WHERE tag.tag_id IN (1,2,3,4,5,6,7,8,9)
+    ORDER BY tag.tag_id ASC,tag.subtag_name ASC`
+    client.query(sql, function (err, results) {
+        if (err) {
+            // 异常后调用callback并传入err
+            res.send({
+                status: 1,
+                message: err.message
+            })
+        } else if (results.rowCount == 0) {
+           
+            // 当前sql查询为空，则返回填报提示
+            res.send({
+                status: 0,
+                message: "该图标无信息"
+            })
+        } else {
+            res.send({
+                status: 0,
+                data: results.rows
+            })
+        }
+    });
+
+}
+
+
 
