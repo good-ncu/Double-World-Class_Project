3f52beccbe1a75031fd6d3947780374924e34932
JasonLou99 <lou1124858363@qq.com>
2022-08-20 14:20:04 +0800

    整体 - 02 - 人才培养基地

--- backend/router/query/gov_query_overall.js
+++ backend/router/query/gov_query_overall.js
@@ -7,8 +7,11 @@ const role_audit = require('../../schema/role_audit')
 
 // 整体 - 01学科建设进展情况
 router.post('/gov-overview-listen-01', role_audit.is_gov, gov_query_overall_handler.gov_overview_listen_01)
+
 // 整体 - 02 - 国家级教学成果奖数量
 router.post('/gov-overview-listen-02-awards', role_audit.is_gov, gov_query_overall_handler.gov_overview_listen_02_awards)
+// 整体 - 02 - 人才培养基地
+router.post('/gov-overview-listen-02-platform', role_audit.is_gov, gov_query_overall_handler.gov_overview_listen_02_platform)
 
 
 module.exports = router
--- backend/router_handler/query/gov_query_overall.js
+++ backend/router_handler/query/gov_query_overall.js
@@ -433,9 +433,81 @@ LIMIT 10`
           })
         } else if (results.rowCount == 0) {
           // 当前sql查询为空，则返回填报提示
-          res.cc("无学科建设进展情况信息")
+          res.cc("无国家级教学成果奖数量信息")
         } else {
           console.log("========gov_overview_listen_02_awards   results_to_data: =========");
+          res.send({
+            status: 0,
+            data: results.rows
+          })
+        }
+      });
+}
+// 整体 - 02 - 人才培养基地
+exports.gov_overview_listen_02_platform = function(req,res){
+    userinfo = req.user
+    sql = `SELECT 
+	concat_ws('-',b.univ_name,b.discipline_name) AS dis_name,
+	SUM(b.plat_num) AS rc_num
+FROM
+(
+SELECT
+		a.univ_code,
+		a.discipline_code,
+		a.univ_name,
+		a.discipline_name,
+		COUNT(talent_platbase_const.id) AS plat_num 	--平台数量
+	FROM
+	((
+	SELECT
+		univ_discipline.univ_code,
+		univ_discipline.discipline_code,
+		univ_discipline.univ_name,
+		univ_discipline.subtag1 AS discipline_name
+	FROM univ_discipline
+	WHERE univ_discipline.tag1='学科群' 
+	)
+	UNION
+	(
+	SELECT
+		univ_discipline.univ_code,
+		univ_discipline.discipline_code,
+		univ_discipline.univ_name,
+		univ_discipline.discipline_name
+	FROM univ_discipline
+	WHERE univ_discipline.tag1='一流学科建设名单'
+	)) AS a
+	INNER JOIN talent_platbase_const 
+		ON a.univ_code = talent_platbase_const.univ_code AND a.discipline_code = talent_platbase_const.discipline_code
+	INNER JOIN user_fill 
+		ON user_fill.id = talent_platbase_const.user_fill_id
+	WHERE 
+		user_fill.is_delete = '0' 
+		AND talent_platbase_const.is_delete = '0' 
+		AND talent_platbase_const.plat_base_level = '国家级'
+	GROUP BY
+		a.univ_code,
+		a.discipline_code,
+		a.univ_name,
+		a.discipline_name
+	) AS b
+GROUP BY 
+	b.univ_name,
+	b.discipline_name
+ORDER BY rc_num DESC
+LIMIT 10`
+    client.query(sql, function (err, results) {
+        if (err) {
+          // 异常后调用callback并传入err
+          res.send({
+            status: 1,
+            message: err.message
+          })
+        } else if (results.rowCount == 0) {
+          // 当前sql查询为空，则返回填报提示
+          res.cc("无人才培养基地信息")
+        } else {
+          console.log("========gov_overview_listen_02_platform   results_to_data: =========");
           res.send({
             status: 0,
             data: results.rows
