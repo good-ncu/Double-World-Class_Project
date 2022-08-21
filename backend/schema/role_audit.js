01e3739c48cd3114697799c2301dd8566607c72c
ZengTao <taozeng66@outlook.com>
2022-08-21 10:12:57 +0800

    增加学校省厅约束 	modified:   backend/schema/role_audit.js

--- backend/schema/role_audit.js
+++ backend/schema/role_audit.js
@@ -3,26 +3,35 @@
     验证角色类别，是否可以访问对应角色的接口
  */
 
-exports.is_gov = function(req,res,next){
-    if(req.user.role_id == 2){
+exports.is_gov = function (req, res, next) {
+    if (req.user.role_id == 2) {
         next()
     } else {
         return res.cc('角色类别错误，无法访问')
     }
 }
 
-exports.is_school = function(req,res,next){
+exports.is_school = function (req, res, next) {
     console.log(req.user.id);
-    if(req.user.role_id == 3){
+    if (req.user.role_id == 3) {
         next()
     } else {
         return res.cc('角色类别错误，无法访问')
     }
 }
 
-exports.is_discipline = function(req,res,next){
+exports.is_discipline = function (req, res, next) {
     console.log(req.user.id);
-    if(req.user.role_id == 4){
+    if (req.user.role_id == 4) {
+        next()
+    } else {
+        return res.cc('角色类别错误，无法访问')
+    }
+}
+
+
+exports.is_gov_or_school = function (req, res, next) {
+    if (req.user.role_id == 2 || req.user.role_id == 3) {
         next()
     } else {
         return res.cc('角色类别错误，无法访问')
