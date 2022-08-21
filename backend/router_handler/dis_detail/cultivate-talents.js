5ff399b56ad38d0772f0580990ba41e4021db772
ZengTao <taozeng66@outlook.com>
2022-08-21 12:21:30 +0800

    test 	modified:   backend/router_handler/dis_detail/cultivate-talents.js

--- backend/router_handler/dis_detail/cultivate-talents.js
+++ backend/router_handler/dis_detail/cultivate-talents.js
@@ -14,6 +14,43 @@ var fs = require('fs');
 
 // 教学成果奖 情况（2-2-1全部）      即 2-2-1-0到2-2-1-3
 exports.gov_detail_2_award = function (req, res) {
+    // 验证token
+    user = req.user
+    // 接收参数
+    subject = req.body.subject
+    // sql填入          ======== 修改
+    sql = ''
+    client.query(sql, function (err, results) {
+        if (err) {
+            // 异常后调用callback并传入err
+            res.send({
+                status: 1,
+                message: err.message
+            })
+        } else if (results.rowCount == 0) {
+            // 当前sql查询为空，则返回填报提示           ========= 修改 标题上的注释抄下来
+            res.cc("该校学科无教学成果奖的信息")
+        } else {
+            //   调试阶段可以 ============修改  gov_detail_2_award 
+            console.log("========gov_detail_2_award   results_to_data: =========");
+            console.log(results.rows);
+            res.send({
+                status: 0,
+                // name: results.rows
+                // ==============修改   返回前对照rap2 字段是否一致
+                name: [
+                    "国家教学成果奖",
+                    "省级教学成果奖",
+                    "研究生教育成果奖"
+                ],
+                value: [
+                    220,
+                    132,
+                    101
+                ]
+            })
+        }
+    });
 
 }
 
