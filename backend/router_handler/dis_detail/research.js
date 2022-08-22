d66b345fca6be1bace63477d7acbe715b3780b13
JasonLou99 <lou1124858363@qq.com>
2022-08-22 09:32:44 +0800

    3个数字  4-1-2   4-1-4  4-2-4   ['开出版专著', '承担国内外重大设计', '主办学术期刊']

--- backend/router_handler/dis_detail/research.js
+++ backend/router_handler/dis_detail/research.js
@@ -371,5 +371,117 @@ exports.gov_detail_4_copaper = function (req, res) {
 
 // 3个数字  4-1-2   4-1-4  4-2-4   ['开出版专著', '承担国内外重大设计', '主办学术期刊']
 exports.gov_detail_4_number = function (req, res) {
-
+    subject = req.body.subject
+    sql = `SElECT
+	concat_ws('-',all_xk.univ_name,all_xk.discipline_name) AS dis_name,
+	sum(COALESCE(a1.num,0)) AS monog_num,	--开出版专著
+	sum(COALESCE(a2.num,0)) AS design_num,	--承担国内外重大设计
+	sum(COALESCE(a3.num,0)) AS jour_num	--主办学术期刊
+FROM
+(
+	(
+	SELECT
+		univ_discipline.univ_code,
+		univ_discipline.discipline_code,
+		univ_discipline.univ_name,
+		univ_discipline.subtag1 AS discipline_name
+	FROM univ_discipline
+	WHERE univ_discipline.tag1='学科群' 
+	)
+	UNION
+	(
+	SELECT
+		univ_discipline.univ_code,
+		univ_discipline.discipline_code,
+		univ_discipline.univ_name,
+		univ_discipline.discipline_name
+	FROM univ_discipline
+	WHERE univ_discipline.tag1='一流学科建设名单'
+	)
+) AS all_xk
+LEFT JOIN
+(
+	SELECT
+		tch_monog.univ_code,
+		tch_monog.discipline_code,
+		count(tch_monog.id) AS num
+	FROM
+		tch_monog
+	INNER JOIN user_fill 
+		ON user_fill.id = tch_monog.user_fill_id
+	WHERE 
+		user_fill.is_delete = '0' 
+		AND tch_monog.is_delete = '0'
+	GROUP BY
+		tch_monog.univ_code,
+		tch_monog.discipline_code
+	) AS a1 ON all_xk.univ_code = a1.univ_code AND all_xk.discipline_code = a1.discipline_code	
+LEFT JOIN
+(
+	SELECT
+		utk_desgshow.univ_code,
+		utk_desgshow.discipline_code,
+		count(utk_desgshow.id) AS num
+	FROM
+		utk_desgshow
+	INNER JOIN user_fill 
+		ON user_fill.id = utk_desgshow.user_fill_id
+	WHERE 
+		user_fill.is_delete = '0' 
+		AND utk_desgshow.is_delete = '0'
+	GROUP BY
+		utk_desgshow.univ_code,
+		utk_desgshow.discipline_code
+	) AS a2 ON all_xk.univ_code = a2.univ_code AND all_xk.discipline_code = a2.discipline_code	
+LEFT JOIN
+(
+	SELECT
+		host_acjour.univ_code,
+		host_acjour.discipline_code,
+		count(host_acjour.id) AS num
+	FROM
+		host_acjour
+	INNER JOIN user_fill 
+		ON user_fill.id = host_acjour.user_fill_id
+	WHERE 
+		user_fill.is_delete = '0' 
+		AND host_acjour.is_delete = '0'
+	GROUP BY
+		host_acjour.univ_code,
+		host_acjour.discipline_code
+	) AS a3 ON all_xk.univ_code = a3.univ_code AND all_xk.discipline_code = a3.discipline_code
+WHERE concat_ws('-',all_xk.univ_name,all_xk.discipline_name)='江西理工大学-冶金工程'	--传参数
+GROUP BY
+	all_xk.univ_name,
+	all_xk.discipline_name`
+    client.query(sql, function (err, results){
+        if (err) {
+            // 异常后调用callback并传入err
+            return res.send({
+                status: 1,
+                message: err.message
+            })
+        } else if (results.rowCount == 0) {
+            // 当前sql查询为空，则返回填报提示           ========= 修改 标题上的注释抄下来
+            return res.send({
+                status: 0,
+                name: [],
+                data: []
+            })
+        } else {
+            console.log(results.rows);
+            var name = ['公开出版专著', '承担国内外重大设计', '主办学术期刊']
+            var data = []
+            results.rows.map(function (item){
+                data.push(item.monog_num)
+				data.push(item.design_num)
+				data.push(item.jour_num)
+            })
+            return res.send({
+                status: 0,
+                name: name,
+                data: data
+            })
+        }
+    })
 }
