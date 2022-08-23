3f0a1e826db42cccb50afef480f0e9bd4ed0494e
JasonLou99 <lou1124858363@qq.com>
2022-08-23 14:25:08 +0800

    -

--- backend/router_handler/gen_report.js
+++ backend/router_handler/gen_report.js
@@ -3,6 +3,7 @@ const PizZip = require("pizzip")
 const fs = require("fs");
 const path = require("path");
 exports.gen_report = function(req,res){
+    console.log(__dirname);
     const content = fs.readFileSync(
         path.resolve(__dirname, "fuwu.docx"),
         "binary"
@@ -13,6 +14,7 @@ exports.gen_report = function(req,res){
         linebreaks: true,
     });
     doc.render({
+        // sql查出的结果在这写入报告中
             year: "2022",
             quarter: "ssssss"
     });
@@ -23,49 +25,4 @@ exports.gen_report = function(req,res){
         compression: "DEFLATE",
     });
     fs.writeFileSync(path.resolve(__dirname, "output.docx"), buf);
-    // // 读取并获得模板文件的二进制内容
-    // JSZipUtils.getBinaryContent('./fuwu.docx', function(err, content){
-    //     if(err){
-    //         throw err
-    //     }
-    //     // 创建一个PizZip实例，内容为模板的内容
-    //     let zip = new PizZip(content)
-    //     // 创建并加载docxtemplater实例对象
-    //     let doc = new docxtemplater().loadZip(zip);
-    //     // 去除未定义值所显示的undefined
-    //     doc.setOptions({
-    //         nullGetter: function () {
-    //             return "";
-    //         }
-    //     });
-    //     doc.setData({
-    //         year: "2022",
-    //         quarter: "第一"
-    //     })
-
-    //     try {
-    //         // 用模板变量的值替换所有模板变量
-    //         doc.render();
-    //     } catch (error) {
-    //         // 抛出异常
-    //         let e = {
-    //             message: error.message,
-    //             name: error.name,
-    //             stack: error.stack,
-    //             properties: error.properties,
-    //         };
-    //         console.log(JSON.stringify({ error: e }));
-    //         throw error;
-    //     }
-
-    //     // 生成一个代表docxtemplater对象的zip文件（不是一个真实的文件，而是在内存中的表示）
-    //     let out = doc.getZip().generate({
-    //         type: "blob",
-    //         mimeType:
-    //             "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    //     });
-    //     // 将目标文件对象保存为目标类型的文件，并命名
-    //     saveAs(out, "testlou.docx");
-    // })
-
 }
