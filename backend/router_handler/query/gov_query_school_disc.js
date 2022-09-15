6fce14895fddda694cdbe25edb35b6c3c0660b68
ZengTao <taozeng66@outlook.com>
2022-09-15 22:29:25 +0800

    省厅 百分比 bug修复

--- backend/router_handler/query/gov_query_school_disc.js
+++ backend/router_handler/query/gov_query_school_disc.js
@@ -112,59 +112,57 @@ exports.gov_query_school_disc_eval = function (req, res) {
 
 
 
-//  省厅 获取指定学校  对应的所有学科
+//  省厅 获取指定学校  对应的所有学科  及其填报进度  （百分比）
 exports.province_subjects_of_school = function (req, res) {
     // userinfo = req.user
     school_name = req.body.school
-    var sql1 = []
-    if (school_name == '南昌大学') {
-        sql1.push(`SELECT ROW_NUMBER () OVER (ORDER BY  subtag_name  DESC) AS  id,subtag_name AS name
-        FROM tag
-        WHERE tag_id<7`)
-    } else {
-        sql1.push(`SELECT ROW_NUMBER () OVER (ORDER BY  discipline_name  DESC) AS  id,univ_discipline.univ_code,univ_name,discipline_code,discipline_name AS name
-        FROM univ_discipline
-        where univ_name ='${school_name}'`)
-    }
-    sql2 = `SELECT
-    b.dis_name,
+    sql = []
+    sql.push(`SELECT
+    b.discipline_name AS dis_name,
     b.already_fill_num,
     COUNT( DISTINCT fill.id)* b.user_num AS need_fill_num
    FROM
    (
    SELECT
-    all_xk.discipline_name AS dis_name,
-    all_xk.univ_name AS univ_name,
-    COUNT( DISTINCT user_fill.user_id) AS user_num,
-    COUNT(user_fill.user_id) AS already_fill_num
+    all_xk.univ_name,
+    all_xk.discipline_name,
+    COUNT(DISTINCT user_info.id) AS user_num,
+    COUNT(a.user_id) AS already_fill_num
    FROM
-    user_info
-   INNER JOIN
-    ((
-    SELECT
-     univ_discipline.univ_code,
-     univ_discipline.discipline_code,
-     univ_discipline.univ_name,
-     univ_discipline.subtag1 AS discipline_name
-    FROM univ_discipline
-    WHERE univ_discipline.tag1='学科群' 
+   (
+    (
+     SELECT
+      univ_discipline.univ_code,
+      univ_discipline.discipline_code,
+      univ_discipline.univ_name,
+      univ_discipline.subtag1 AS discipline_name
+     FROM univ_discipline
+     WHERE univ_discipline.tag1='学科群' 
     )
     UNION
     (
-    SELECT
-     univ_discipline.univ_code,
-     univ_discipline.discipline_code,
-     univ_discipline.univ_name,
-     univ_discipline.discipline_name
-    FROM univ_discipline
-    WHERE univ_discipline.tag1='一流学科建设名单'
-    )) AS all_xk ON all_xk.discipline_code = user_info.discipline_code AND all_xk.univ_code = user_info.univ_code
-   INNER JOIN user_fill ON user_fill.user_id = user_info.id
+     SELECT
+      univ_discipline.univ_code,
+      univ_discipline.discipline_code,
+      univ_discipline.univ_name,
+      univ_discipline.discipline_name
+     FROM univ_discipline
+     WHERE univ_discipline.tag1='一流学科建设名单'
+    )
+   ) AS all_xk  
+   LEFT JOIN user_info 
+    ON all_xk.discipline_code = user_info.discipline_code AND all_xk.univ_code = user_info.univ_code
+   LEFT JOIN 
+   (
+    SELECT 
+     user_fill.user_id
+    FROM user_fill
+    WHERE user_fill.fill_id IN (SELECT id FROM fill WHERE fill.flag=1)
+     AND user_fill.is_delete = '0'
+     AND user_fill.flag = 1
+   ) AS a ON a.user_id = user_info.id
    WHERE 
-    all_xk.univ_name IN('${school_name}')
-    AND user_fill.fill_id IN (SELECT id FROM fill WHERE fill.flag=1)
-    AND user_fill.is_delete = '0'
-    AND user_fill."flag" = 1
+    all_xk.univ_name = '${school_name}' --传参数
    GROUP BY
     all_xk.univ_name,
     all_xk.discipline_name
@@ -172,11 +170,11 @@ exports.province_subjects_of_school = function (req, res) {
    CROSS JOIN fill
    WHERE fill.flag = 1
    GROUP BY
-    b.dis_name,
+    b.discipline_name,
     b.user_num,
-    b.already_fill_num`
-    var fist_out = []
-    async.eachSeries(sql1, function (item, callback) {
+    b.already_fill_num`)
+    return_data = []
+    async.eachSeries(sql, function (item, callback) {
         // 遍历每条SQL并执行
         client.query(item, function (err, results) {
             if (err) {
@@ -189,9 +187,11 @@ exports.province_subjects_of_school = function (req, res) {
                 callback(err)
             } else {
                 for (let i = 0, len = results.rows.length; i < len; i++) {
-                    fist_out[i] = results.rows[i]
-                    fist_out[i].percentage = '0%'
+                    results.rows[i].percentage = Math.ceil((results.rows[i].already_fill_num) / (results.rows[i].need_fill_num) * 100)
+                    results.rows[i].percentage = results.rows[i].percentage + '%'
+                    console.log(results.rows[i])
                 }
+                return_data = results.rows
                 callback()
             }
         });
@@ -200,28 +200,12 @@ exports.province_subjects_of_school = function (req, res) {
             console.log(err);
             return res.cc(err)
         } else {
-            client.query(sql2, function (err, results) {
-                if (err) {
-                    // 异常后调用callback并传入err
-                    err = "系统错误，请刷新页面后重试"
-                    return res.cc(err)
-                } else {
-                    fist_out.percentage = '0%'
-                    for (let i = 0, len = results.rows.length; i < len; i++) {
-                        for (let j = 0, len = fist_out.length; j < len; j++) {
-                            if (results.rows[i].dis_name == fist_out[j].name) {
-                                fist_out[j].percentage = Math.ceil((results.rows[i].already_fill_num) / (results.rows[i].need_fill_num) * 100)
-                                fist_out[j].percentage = fist_out[j].percentage + '%'
-                                break
-                            }
-                        }
-                    }
-                    res.send({
-                        status: 0,
-                        data: fist_out
-                    })
-                }
-            });
+
+            res.send({
+                status: 0,
+                data: return_data
+            })
+
         }
     })
 }
