d86496e3075ade92cfee3ffeb8ac6224c93568c3
JasonLou99 <lou1124858363@qq.com>
2022-10-11 23:40:05 +0800

    -

--- backend/router_handler/query/download_attachment.js
+++ backend/router_handler/query/download_attachment.js
@@ -6,28 +6,43 @@ const urlencode = require('urlencode')
 
 exports.attach_query_download = function(req,res){
     id = req.body.id
-    user = req.user
-    console.log(user);
-    // 查询sql，以最新的记录为准
-    sql = `select * from attachment where user_id = '${user.id}' and fill_id = '${id}' order by create_time desc limit 1`
-    client.query(sql,function(err,results){
+    univ_code = req.user.univ_code
+    console.log(univ_code);
+    discipline_name = req.body.name
+    // 查询 拼装用户的user_id
+    sql1 = `select * from univ_discipline where univ_code = '${univ_code}' and discipline_name = '${discipline_name}'`
+    client.query(sql1,function(err,results){
         if(err){
             console.error(err);
             return res.cc("系统繁忙，请稍后再试")
         } else {
             if (results.rowCount==0){
-                return res.send({
-                    status: 0,
-                    filenameList: []
-                })
+                return res.cc('无该学校学科信息')
             }
-            path_dir = results.rows[0].path
-            // path_dir = 'D:\\temp_upload'
-            // dirs: path_dir下的所有文件
-            dirs = fs.readdirSync(path_dir)
-            return res.send({
-                status: 0,
-                filenameList: dirs
+            discipline_code = results.rows[0].discipline_code
+            user_id = univ_code+'_'+discipline_code
+            // 查询sql，以最新的记录为准
+            sql2 = `select * from attachment where user_id = '${user_id}' and fill_id = '${id}' order by create_time desc limit 1`
+            client.query(sql2, function(err, results){
+                if(err){
+                    console.error(err);
+                    return res.cc("系统繁忙，请稍后再试")
+                } else {
+                    if (results.rowCount==0){
+                        return res.send({
+                            status: 0,
+                            filenameList: []
+                        })
+                    }
+                    path_dir = results.rows[0].path
+                    // path_dir = 'D:\\temp_upload'
+                    // dirs: path_dir下的所有文件
+                    dirs = fs.readdirSync(path_dir)
+                    return res.send({
+                        status: 0,
+                        filenameList: dirs
+                    })
+                }
             })
         }
     })
