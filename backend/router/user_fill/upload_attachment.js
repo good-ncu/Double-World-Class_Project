645715f2bbca20e0a53f5df3933078ccb3e9776b
JasonLou99 <lou1124858363@qq.com>
2022-10-23 12:06:05 +0800

    upload-attachment 添加中间件解决跨域问题

--- backend/router/user_fill/upload_attachment.js
+++ backend/router/user_fill/upload_attachment.js
@@ -5,7 +5,13 @@ const upload_attachment = require('../../router_handler/user_fill/upload_attachm
 const pre_view_handler = require('../../router_handler/preview_table')
 const public = require('../../excel_parsing/公用中间件multer')
 
-router.post('/upload-attachment',public.tempupload_word_file_callback,pre_view_handler.preview_attachment)
+router.post('/upload-attachment',
+    function(req,res,next){
+        res.set('Access-Control-Allow-Origin','*')
+        next()
+    },
+    public.tempupload_word_file_callback,
+    pre_view_handler.preview_attachment)
 router.post('/upload-attachment-sub',upload_attachment.upload_sub)
 
 module.exports = router
