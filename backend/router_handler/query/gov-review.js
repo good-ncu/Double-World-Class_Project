4b4ed8f7c7336298fce5e986ed5feb214a5143f4
ZengTao <taozeng66@outlook.com>
2022-10-31 22:06:20 +0800

    省厅查看功能 	modified:   backend/router/query/gov-review.js 	modified:   backend/router_handler/query/gov-review.js

--- backend/router_handler/query/gov-review.js
+++ backend/router_handler/query/gov-review.js
@@ -239,8 +239,6 @@ exports.download_all_data = function (req, res) {
 }
 
 
-
-
 function deal_data(data) {
     for (var i = 0; i < data.length; i++) {
         delete data[i].id
@@ -302,4 +300,88 @@ function test(data, head, dict, workbook, univ_code, discipline_code) {
             };
         };
     }
+}
+
+
+
+
+
+
+/**
+ * 该handler用于 省厅查看！！！
+ * 需要传入该张表的信息  fill_id
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.query_single_table_info = function (req, res) {
+    var userinfo = req.user
+    var subinfo = req.body
+    console.log(subinfo.fill_id)
+    console.log(subinfo.univ_name)
+    console.log(subinfo.discipline_name)
+
+    // 拿到具体的数据库表名to_dbtable 以及 表中对应数据的  id  (改成user_fill_id)
+    var sql1 = `select user_fill.id AS user_fill_id , fill.to_dbtable AS to_dbtable
+    from user_fill,fill
+    where user_fill.fill_id = '${subinfo.fill_id}' AND fill.id='${subinfo.fill_id}' AND user_fill.flag = 1 AND user_fill.is_delete = 0  AND user_fill.user_id = (SELECT user_info.id from user_info where user_info.univ_name = '${subinfo.univ_name}' AND user_info.discipline_name = '${subinfo.discipline_name}') `
+    client.query(sql1, function (err, results) {
+        if (err) {
+            console.err(err)
+            // 异常后调用callback并传入err
+            res.send({
+                status: 1,
+                message: '系统繁忙，请稍后再试'
+            })
+        } else if (results.rowCount == 0) {
+
+            // 当前sql查询为空，则返回填报提示
+            res.send({
+                status: 1,
+                message: "该表格本周期您还未填报数据!!"
+            })
+        } else {
+            console.log(sql1)
+            var user_fill_id = results.rows[0].user_fill_id
+            var to_dbtable = results.rows[0].to_dbtable
+            console.log(user_fill_id)
+            console.log(to_dbtable)
+            var sql2 = `select * from ${to_dbtable} where user_fill_id = '${user_fill_id}' AND is_delete = 0`
+            client.query(sql2, function (err, results2) {
+                if (err) {
+                    console.log(err.message)
+                    // 异常后调用callback并传入err
+                    res.send({
+                        status: 1,
+                        message: '系统繁忙，请稍后再试'
+                    })
+                } else if (results.rowCount == 0) {
+                    // 当前sql查询为空，则返回填报提示
+                    return res.cc('该表格下无填报的数据')
+                } else {
+                    console.log(results2.rows);
+                    for (var i = 0; i < results2.rows.length; i++) {
+                        if (results2.rows[i]["adopt_date"] == 'undefined') {
+                            results2.rows[i]["adopt_date"] = ""
+                        }
+                        // results2.rows[i]["is_seen"] = null
+                        // results2.rows[i]["is_delete"] = null
+                        // results2.rows[i]["path"] = null
+                        delete results2.rows[i]["is_seen"]
+                        delete results2.rows[i]["is_delete"]
+                        delete results2.rows[i]["path"]
+                        delete results2.rows[i]["id"]
+                        delete results2.rows[i]["op_time"]
+                        delete results2.rows[i]["user_fill_id"]
+                    }
+                    res.send({
+                        status: 0,
+                        data: results2.rows
+                    })
+                }
+
+            })
+        }
+    });
+
+
 }
